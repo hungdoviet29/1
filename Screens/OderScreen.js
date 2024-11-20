a78a0828112fb877d85b3feb8c4526c846a0f641
Hoang231024 <hoang0962442523@gmail.com>
2024-11-20 20:52:38 +0700

    fix server

--- Screens/OderScreen.js
+++ Screens/OderScreen.js
@@ -20,15 +20,15 @@ const OrderScreen = ({ navigation }) => {
   const [refreshTrigger, setRefreshTrigger] = useState(false);
 
   const statusMap = {
-    'Tất cả': (id) => `http://192.168.3.106:3000/donhang/user/${id}`,
+    'Tất cả': (id) => `http://172.20.10.6:3000/donhang/user/${id}`,
     'Chờ xác nhận': (id) =>
-      `http://192.168.3.106:3000/donhang/user/${id}/status?status=${encodeURIComponent('Chờ xác nhận')}`,
+      `http://172.20.10.6:3000/donhang/user/${id}/status?status=${encodeURIComponent('Chờ xác nhận')}`,
     'Chờ vận chuyển': (id) =>
-      `http://192.168.3.106:3000/donhang/user/${id}/status?status=${encodeURIComponent('Chờ vận chuyển')}`,
+      `http://172.20.10.6:3000/donhang/user/${id}/status?status=${encodeURIComponent('Chờ vận chuyển')}`,
     'Đang vận chuyển': (id) =>
-      `http://192.168.3.106:3000/donhang/user/${id}/status?status=${encodeURIComponent('Đang vận chuyển')}`,
+      `http://172.20.10.6:3000/donhang/user/${id}/status?status=${encodeURIComponent('Đang vận chuyển')}`,
     'Đã hủy': (id) =>
-      `http://192.168.3.106:3000/donhang/user/${id}/status?status=${encodeURIComponent('Đã hủy')}`,
+      `http://172.20.10.6:3000/donhang/user/${id}/status?status=${encodeURIComponent('Đã hủy')}`,
   };
 
   // Lấy userId từ AsyncStorage
@@ -89,7 +89,7 @@ const OrderScreen = ({ navigation }) => {
 
   const cancelOrder = async (orderId) => {
     try {
-      const response = await fetch(`http://192.168.3.106:3000/donhang/${orderId}`, {
+      const response = await fetch(`http://172.20.10.6:3000/donhang/${orderId}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ status: 'Đã hủy' }),
@@ -110,7 +110,7 @@ const OrderScreen = ({ navigation }) => {
 
   const confirmReceived = async (orderId) => {
     try {
-      const response = await fetch(`http://192.168.3.106:3000/donhang/${orderId}`, {
+      const response = await fetch(`http://172.20.10.6:3000/donhang/${orderId}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ status: 'Thành Công' }),
