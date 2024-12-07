dda55601d4f7ab47a602b842a70e46ec79d3a58c
Giappp2004 <150682043+Giappp2004@users.noreply.github.com>
2024-12-07 16:08:20 +0700

    vocher update
    vocher update

--- server/models/voucher.js
+++ server/models/voucher.js
@@ -4,8 +4,11 @@ const voucherSchema = new mongoose.Schema({
   type: {type: String, required: true}, // Loại voucher
   title: {type: String, required: true}, // Tiêu đề voucher
   description: {type: String, required: true}, // Mô tả voucher
-  expirationDate: {type: String, required: true}, // Ngày hết hạn (String hoặc Date)
+  startDate: {type: Date, required: true}, // Ngày bắt đầu
+  expirationDate: {type: Date, required: true}, // Ngày hết hạn
   isActive: {type: Boolean, default: true}, // Trạng thái voucher
+  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, // ID người dùng
+  quantity: {type: Number, required: true}, // Số lượng
 });
 
 const Voucher = mongoose.model('Voucher', voucherSchema);
