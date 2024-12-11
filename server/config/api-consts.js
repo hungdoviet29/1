2e76d283a3b0f19856c9967cd45073742a745dd1
Hoang231024 <hoang0962442523@gmail.com>
2024-12-12 02:10:17 +0700

    update

---
+++ server/config/api-consts.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    ZALO_PAY_APP_ID: 'your-app-id',
+    ZALO_PAY_SECRET_KEY: 'your-secret-key',
+    ZALO_PAY_MERCHANT_ID: 'your-merchant-id',
+    ZALO_PAY_API_URL: 'https://sb-openapi.zalopay.vn/v2/createorder'
+  };
